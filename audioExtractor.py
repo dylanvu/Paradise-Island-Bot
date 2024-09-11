a2ba5fd7fd644186497a48110582f188b9230546
Jayson Nguyen <jayngu357@gmail.com>
2024-09-11 00:57:29 -0700

    prototype for speaker diarization & noise reduction

--- audioExtractor.py
+++ audioExtractor.py
@@ -1,11 +1,16 @@
 import os
+from dotenv import load_dotenv
 import moviepy.editor as mp 
 import speech_recognition as sr 
+from pyannote.audio import Pipeline
+import noisereduce as nr
+import librosa
+import soundfile as sf
 
 class audioExtract:
     def __init__(self, audioFile) -> None:
         self.audioFile = self.loadVideo(audioFile)
-        print("Filename:", self.audioFile)
+        print("\nFilename:", self.audioFile, "\n")
         # Initialize recognizer 
         self.recognizer = sr.Recognizer()
    
@@ -29,20 +34,60 @@ class audioExtract:
 
         return filename
         
+    """Reduce background noise from audio"""
+    def reduceNoise(self, outputAudio):
+        print(f"Reducing Noise for {self.audioFile}")
+
+        # Load the audio file
+        y, sr = librosa.load(self.audioFile, sr=None)
+        
+        # Reduce noise using noisereduce
+        reduced_noise = nr.reduce_noise(y=y, sr=sr)
+        
+        # Save the noise-reduced audio
+        sf.write(outputAudio, reduced_noise, sr)
+        self.audioFile = outputAudio
+
+
     """Converts wav file audio to text"""
     def convertToText(self) -> str:
+        print("Converting Text")
         # Load the audio file 
         with sr.AudioFile(self.audioFile) as source: 
             data = self.recognizer.record(source) 
     
         # Convert speech to text 
-        return self.recognizer.recognize_google(data)     
+        return self.recognizer.recognize_google(data)    
+
+    def splitSpeakers(self):
+        print("\nSplitting Speakers\n")
+        # Initialize the pre-trained diarization pipeline
+        pipeline = Pipeline.from_pretrained("pyannote/speaker-diarization-3.1", use_auth_token=os.getenv("HUGGINGFACE_TOKEN"))
+
+        # Apply the pipeline on your audio file
+        diarization = pipeline(self.audioFile)
+
+        # Print the diarization result
+        for turn, _, speaker in diarization.itertracks(yield_label=True):
+            print(f"Speaker {speaker} starts at {turn.start} and ends at {turn.end}")
+
 
 if __name__ == "__main__":
+    # loading variables from .env file
+    load_dotenv() 
+
+    # create class object
     extract = audioExtract("audio_files/Contestant Intros Many Speakers.mp4")
     # extract = audioExtract("audio_files/harvard.wav")
-    text = extract.convertToText()
+
+    # Reduce Noise 
+    # extract.reduceNoise("audio_files/Contestant Intros Many Speakers_reduce.wav")
+    # extract.reduceNoise("audio_files/harvard_reduce.wav")
 
     # Print the text 
-    print("\nThe resultant text from video is:\n") 
-    print(text) 
+    # text = extract.convertToText()
+    # print("\nThe resultant text from video is:\n") 
+    # print(text) 
+
+    # Split Speakers
+    extract.splitSpeakers()
